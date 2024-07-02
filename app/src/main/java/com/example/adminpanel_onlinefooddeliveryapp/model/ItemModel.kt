50afa5c45cba3b778039a0c340f94d3b86fc3abb
DELL <abhi.jain9915@gmail.com>
2024-07-02 16:51:44 +0530

    Item Model

---
+++ app/src/main/java/com/example/adminpanel_onlinefooddeliveryapp/model/ItemModel.kt
@@ -0,0 +1,9 @@
+package com.example.adminpanel_onlinefooddeliveryapp.model
+
+data class ItemModel(
+    val foodName:String?= null,
+    val foodPrice:String?= null,
+   val foodDescription:String?= null,
+    val foodIngredients:String?= null,
+     val foodImage:String?= null,
+)
